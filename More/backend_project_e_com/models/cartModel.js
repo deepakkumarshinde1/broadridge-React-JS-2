52cad7201c0d41ff6c985f1f738fdb0f26677ba5
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2025-09-12 10:21:51 +0530

    Backend App

---
+++ More/backend_project_e_com/models/cartModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const CartItem = new mongoose.Schema({
+  productId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Product",
+    required: true,
+  },
+  quantity: { type: Number, default: 1 },
+});
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    items: [CartItem],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.models.Cart || mongoose.model("Cart", CartSchema);
